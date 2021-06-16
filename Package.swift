fca2047c0235040852bf36a196934ec327ba8104
Nikolai Shelekhov <nickshv13@gmail.com>
2021-06-16 09:56:40 +0800

    MacOS version changed

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "stox",
     platforms: [
-        .macOS(.v10_15)
+        .macOS(.v11)
     ],
     products: [
         .executable(name: "stox", targets: ["stox"])
